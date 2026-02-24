053f76b52023d17d2d3a1ee58fa086ffbc3c061e
Abir Shahriar <ashariar414@gmail.com>
2026-02-24 23:21:59 +0600

    one more

---
+++ Module1/class19_multi_array_problems/c19_p2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>  //for abs()
+int main(){
+    // we find diagonal values when i = j, but we will need two loops, there's a way to work with only one loop
+    int n;
+    scanf("%d",&n);
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    int md=0 , sd=0;
+    for (int i=0;i<n;i++){
+        md+=arr[i][i];
+    }
+    for(int i=n-1, j=0;i>=0;i--,j++){  //we could have done i>=0 && j<n but doing i>=0,j<n completely ignores i>=0
+        sd+=arr[i][j];
+    }
+    int ans = abs(md-sd);
+    printf("%d\n",ans);
+    return 0;
+}
